c5988647f0d2c3fa1a9723f12678a25c52003b91
StamLord <42681607+StamLord@users.noreply.github.com>
2021-07-24 19:24:03 +0300

    Camera Movement Feedback
    * MouseLook - Switch to input detection with InputState and add Z axis rotation for movement feedback

--- Ninja/Assets/Scripts/Misc/MouseLook.cs
+++ Ninja/Assets/Scripts/Misc/MouseLook.cs
@@ -7,16 +7,18 @@ public class MouseLook : MonoBehaviour
     [SerializeField] private Vector2 sensitivity = new Vector2(100f,80f);
     [SerializeField] private Transform playerBody;
 
-    public enum LookState {TURN_BODY, TURN_HEAD}
-    [SerializeField] private LookState _lookState = LookState.TURN_BODY;
+    //[SerializeField] private LookState _lookState = LookState.TURN_BODY;
 
     float xRotation = 0f;
+
+    [SerializeField] private InputState inputState;
+    [SerializeField] private float maxRotZ = 1f;
+    [SerializeField] private float rotSpeedZ = 2f;
 
     [SerializeField] private bool disabled;
 
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
         UIManager.OnDisableMouse += Disable;
         UIManager.OnEnableMouse += Enable;
     }
@@ -25,28 +27,28 @@ public class MouseLook : MonoBehaviour
     {
         if(disabled) return;
 
+        // Get Input Rotations
         float mouseX = Input.GetAxis("Mouse X") * sensitivity.x;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity.y;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // Invert
+        this.xRotation -= mouseY;
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // Clamp
+        this.xRotation = Mathf.Clamp(this.xRotation, -90f, 90f);
+
+        // Movement Feedback
+        Vector3 inputVector = inputState.AxisInput;
+        float xRotation = Mathf.Lerp(
+            transform.localRotation.z, 
+            (inputVector.x > 0)? -maxRotZ : (inputVector.x < 0) ? maxRotZ : 0f, 
+            Mathf.Abs(inputVector.x) * rotSpeedZ);
+
+        // Rotate Camera
+        transform.localRotation = Quaternion.Euler(this.xRotation, 0f, xRotation);
         
-        switch(_lookState)
-        {
-            case LookState.TURN_BODY:
-            playerBody.Rotate(Vector3.up * mouseX);
-            break;
-
-            case LookState.TURN_HEAD:
-            break;
-        }
-    }
-
-    void SetLookState(LookState lookState)
-    {
-        _lookState = lookState;
+        // Rotate transform
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 
     private void Enable()
