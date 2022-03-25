dbbc18b197a003d25bec1e1f14f3593149d80364
StamLord <42681607+StamLord@users.noreply.github.com>
2022-03-25 12:18:05 +0300

    TestScene.unity
    * TestScene.unity - Colored course red, added curved wall running section and course respawn area
    * FallRespawn - Added OnTriggerEnter respawn

--- Ninja/Assets/Scripts/Misc/FallRespawn.cs
+++ Ninja/Assets/Scripts/Misc/FallRespawn.cs
@@ -6,11 +6,12 @@ public class FallRespawn : MonoBehaviour
 {
     [SerializeField] private Rigidbody player;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private bool heightBased = false;
     [SerializeField] private float minimumWorldY;
 
     void FixedUpdate()
     {
-        if(player.transform.position.y < minimumWorldY)
+        if(heightBased && player.transform.position.y < minimumWorldY)
             Respawn();
     }
 
@@ -18,5 +19,13 @@ public class FallRespawn : MonoBehaviour
     {
         player.velocity = Vector3.zero;
         player.transform.position = spawnPoint.position;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.transform.root == player.transform.root)
+        {
+            Respawn();
+        }
     }
 }
