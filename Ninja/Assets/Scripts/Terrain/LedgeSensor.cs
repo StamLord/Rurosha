343a8457d612eda7fc81ece4f17bd8f2cd142d82
StamLord <42681607+StamLord@users.noreply.github.com>
2022-04-02 12:02:05 +0300

    LedgeSensor - LayerMask added
    * LayerMask added to control what is considered as a ledge

--- Ninja/Assets/Scripts/Terrain/LedgeSensor.cs
+++ Ninja/Assets/Scripts/Terrain/LedgeSensor.cs
@@ -14,7 +14,8 @@ public class LedgeSensor : MonoBehaviour
     [Space(20f)]
 
     [Header("Detection Settings")]
-    [SerializeField] private float ledgeOffset; 
+    [SerializeField] private float ledgeOffset;
+    [SerializeField] private LayerMask ledgeMask;  
 
     [Space(20f)]
 
@@ -33,12 +34,12 @@ public class LedgeSensor : MonoBehaviour
         RaycastHit ledge;
 
         // Make sure we don't face a wall
-        if(Physics.Raycast(transform.position + Vector3.up - Vector3.up * .1f, transform.forward, ledgeOffset))
+        if(Physics.Raycast(transform.position + Vector3.up - Vector3.up * .1f, transform.forward, ledgeOffset, ledgeMask))
             return;
 
         Ray ledgeRay = new Ray(transform.position + transform.forward * ledgeOffset + Vector3.up - Vector3.up * .1f, Vector3.down);
         
-        ledgeDetected = Physics.Raycast(ledgeRay, out ledge, 1f);
+        ledgeDetected = Physics.Raycast(ledgeRay, out ledge, 1f, ledgeMask);
         ledgePoint = ledge.point;
     }
 
