f16ba0291b10b105dc77090fa62eb1f3ca672826
StamLord <42681607+StamLord@users.noreply.github.com>
2022-06-23 01:28:58 +0300

    Yuki-Onna - Enemy & Ragdoll test
    * RagdollCopy.cs - Attach to armature to copy all bones from a different armature

---
+++ Ninja/Assets/Scripts/ProceduralAnimation/RagdollCopy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollCopy : MonoBehaviour
+{
+    [SerializeField] private Transform reference;
+    
+    private void OnEnable() 
+    {
+        CopyBone(transform, reference);
+    }
+
+    private void CopyBone(Transform target, Transform reference)
+    {
+        Debug.Log(target + " <= " + reference);
+        target.localPosition = reference.localPosition;
+        target.localRotation = reference.localRotation;
+
+        int lowestBoneNum = Mathf.Min(target.childCount, reference.childCount);
+
+        for(int i = 0; i < lowestBoneNum; i++)
+        {
+            Transform t = target.GetChild(i);
+            Transform r = reference.GetChild(i);
+            if(t != null && r != null)            
+                CopyBone(t, r);
+        }
+    }
+}
