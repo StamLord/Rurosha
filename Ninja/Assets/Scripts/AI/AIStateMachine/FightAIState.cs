60b75ba0aa88b1421667a1d8bd30cc994b23d35b
StamLord <42681607+StamLord@users.noreply.github.com>
2022-07-04 01:58:08 +0300

    Update FightAIState.cs
    * Added aggressive, maxCombo, attackInterval, dontAttackInterval parameters to behavior

--- Ninja/Assets/Scripts/AI/AIStateMachine/FightAIState.cs
+++ Ninja/Assets/Scripts/AI/AIStateMachine/FightAIState.cs
@@ -4,18 +4,23 @@ using System.Collections;
 public class FightAIState : AIState
 {
     [Header("Movement")]
-    [SerializeField] private float enemyDistance = 1f;
+    [SerializeField] private float enemyDistance = 3f;
     [SerializeField] private float pathRecalculateDistance = 3f;
     [SerializeField] private float faceEnemyDistance = 5f;
 
     [Header("Attack")]
     [SerializeField] private bool defensive;
-    [SerializeField] private float defenseRange = 3f;
+    [SerializeField] private float defenseRange = 5f;
+    [Range(1,5)] [SerializeField] private int aggressive;
+    [SerializeField] private float dontAttackInterval = 2f;
+    [SerializeField] private bool attackCombo;
+    [SerializeField] private int attackMaxCombo = 3;
+    [SerializeField] private float attackInterval = 3f;
 
     private StealthAgent enemy;
     private Vector3 lastPathTarget;
     private Coroutine attackCoroutine;
-    public bool attacking;
+    public bool canAttack = true;
     public bool defending;
 
     protected override void OnEnterState()
@@ -28,6 +33,7 @@ public class FightAIState : AIState
 
         // Draw weapon
         PressButton("2");
+        canAttack = true;
 
         if(debug)
             AIStateMachine.SetDebugColor(Color.red);
@@ -50,24 +56,28 @@ public class FightAIState : AIState
             MoveTo(enemy.transform.position);
         }
         
-        if (inAtkRange && attacking == false) // Attack if in range and not already attacking
+        if (inAtkRange &&   // Attack if in range and 
+            canAttack)      // Not already attacking
         {
-            StopDefend();
-            attackCoroutine = StartCoroutine("Attack", .5f);
+            // Aggressive chance roll
+            int agg = Random.Range(0, aggressive); // Returns 0 .. 4, at aggressive = 1 we never attack, at aggressive = 5 we attack 4/5
+            if(agg == 0)
+                attackCoroutine = StartCoroutine("DontAttack", dontAttackInterval);
+            else
+            {            
+                StopDefend();
+                attackCoroutine = StartCoroutine("Attack", attackInterval);
+                return;
+            }
         }
         
-        if(defensive) 
+        if(defensive)
         {
             bool inDefRange = Vector3.Distance(transform.position, enemy.transform.position) < defenseRange;
             if(inDefRange)
-            {    
-                if(attacking == false)
-                    StartDefend();
-            }
+                StartDefend();
             else
-            {
                 StopDefend();
-            }
         }
     }
 
@@ -107,11 +117,26 @@ public class FightAIState : AIState
 
     private IEnumerator Attack(float waitAfterAttack)
     {
-        attacking = true;
-        PressButton("MB1");
-        //MoveTo(enemy.transform.position);
+        canAttack = false;
+        int attacks = (attackCombo)? attackMaxCombo : 1;
+
+        for (var i = 0; i < attacks; i++)
+        {
+            PressButton("MB1");
+            //MoveTo(enemy.transform.position);
+            if(i < attacks - 1)
+                yield return new WaitForSeconds(.5f);
+        }
+
         yield return new WaitForSeconds(waitAfterAttack);
-        attacking = false;
+        canAttack = true;
+    }
+
+    private IEnumerator DontAttack(float wait)
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(wait);
+        canAttack = true;
     }
     
     private void OnDrawGizmos() 
