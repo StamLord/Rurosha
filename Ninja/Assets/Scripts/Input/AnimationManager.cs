b2d8201ff70a89ef01626d47d293de931188f629
StamLord <42681607+StamLord@users.noreply.github.com>
2022-07-11 00:15:49 +0300

    Player - Visual model & animations
    * AnimationManager.cs - Updated to use rigidbody velocity
    * Player Object prefab - Added visual model and animator

--- Ninja/Assets/Scripts/Input/AnimationManager.cs
+++ Ninja/Assets/Scripts/Input/AnimationManager.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class AnimationManager : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private InputState inputState;
+    [SerializeField] private new Rigidbody rigidbody;
+    [SerializeField] private float runSpeed = 10;
 
-    void LateUpdate()
+    private void OnValidate() 
     {
-        if(animator)
-        {
-            animator.SetFloat("Speed X", inputState.AxisInput.x);
-            animator.SetFloat("Speed Z", inputState.AxisInput.z);
-            animator.SetBool("Crouching", inputState.Crouch.State == VButtonState.PRESSED);
-        }
+        animator = GetComponent<Animator>();
+    }
+    
+    private void LateUpdate()
+    {
+        Vector3 velocity = rigidbody.velocity.magnitude * inputState.AxisInput;
+        animator.SetFloat("x", velocity.x);
+        animator.SetFloat("z", velocity.z / runSpeed);
+        animator.SetBool("crouch", inputState.Crouch.State == VButtonState.PRESSED);
     }
 }
