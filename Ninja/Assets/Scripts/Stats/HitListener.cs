4f9ee1e9fecc729d0e0d9a4b68326cb87fd78feb
StamLord <42681607+StamLord@users.noreply.github.com>
2023-01-01 19:07:46 +0200

    HitListener created
    * HitListener - Executes unity events when OnHit is called by CharacterStats

---
+++ Ninja/Assets/Scripts/Stats/HitListener.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class HitListener : MonoBehaviour
+{
+    [SerializeField] private CharacterStats characterStats;
+    [SerializeField] private UnityEvent action;
+
+    void Start()
+    {
+        if(characterStats) characterStats.OnHit += Execute;
+    }
+
+    private void Execute(int softDamage, int hardDamage)
+    {
+        action.Invoke();
+    }
+}
