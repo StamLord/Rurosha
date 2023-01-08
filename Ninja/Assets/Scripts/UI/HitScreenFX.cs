05b01f47e9dfb6a7890905846a9ad014140ee2ea
StamLord <42681607+StamLord@users.noreply.github.com>
2023-01-08 23:51:49 +0200

    Hit Screen FX
    * HitScreenFX - Plays coroutine to show and fade away a screen sized texture
    * Textures/UI/vignette.png - Vingette texture 1920x1080
    * Hit Vignette.prefab - Prefab to be used on Canvas to add a hit fx

---
+++ Ninja/Assets/Scripts/UI/HitScreenFX.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HitScreenFX : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] private Image image;
+    
+    public void Play()
+    {
+        StartCoroutine("PlayCoroutine");
+    }
+
+    private IEnumerator PlayCoroutine()
+    {
+        float timeStarted = Time.time;
+        Color imageColor = image.color;
+
+        while(Time.time - timeStarted <= duration)
+        {
+            float p = (Time.time - timeStarted) / duration;
+            imageColor.a = 1 - p;
+            image.color =  imageColor;
+
+            yield return null;
+        }
+
+        imageColor.a = 0;
+        image.color =  imageColor;
+    }
+}
