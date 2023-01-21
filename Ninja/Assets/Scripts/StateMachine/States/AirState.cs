3e1e89003858b7110a72fed2544ad5321948be79
StamLord <42681607+StamLord@users.noreply.github.com>
2023-01-21 20:45:47 +0200

    AirStata.cs - Fall Damage & Glide update

--- Ninja/Assets/Scripts/StateMachine/States/AirState.cs
+++ Ninja/Assets/Scripts/StateMachine/States/AirState.cs
@@ -45,7 +45,7 @@ public class AirState : PlayerState
     [Header("Fall Damage")]
     [SerializeField] private float miniFallDamageVelocity = 2f;
     [SerializeField] private int FallDamage = 10;
-    [SerializeField] private float minimumFallDamageDistance = 10;
+    [SerializeField] private AttributeDependant<float> fallDamageHeight;
     [SerializeField] private float minRollHeight = 2f;
     [SerializeField] private float rollWindow = .1f;
     private float startY;
@@ -55,7 +55,11 @@ public class AirState : PlayerState
     [Header("Glide")]
     [SerializeField] public bool glideOn = false;
     [SerializeField] private float glideGravityMultiplier = .2f;
+    [SerializeField] private float liftForce = 5f;
     [SerializeField] private bool isGliding = false;
+    [SerializeField] private Transform glideTransform;
+    [SerializeField] private float maxTurnSpeed;
+    [SerializeField] private AnimationCurve forwardCurve;
 
     [Space(20f)]
     [Header("Air Jumps")]
@@ -82,6 +86,8 @@ public class AirState : PlayerState
     {
         base.OnEnterState();
         if(debugView) Debug.Log("State: Entered [Air State]");
+
+        colliderManager.SetLegs(ColliderManager.BodyCollider.AIR);
 
         startY = transform.position.y;
         originalSpeed =  new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
@@ -129,60 +135,61 @@ public class AirState : PlayerState
             return;
         }
 
-        // Accelration Method
-        //rigidbody.AddForce(targetVelocity * airControl, ForceMode.VelocityChange);
-        
-        // Instant Method
-        targetVelocity *= airControl;
-
-        // Apply a force that attempts to reach our target velocity
-        Vector3 velocity = rigidbody.velocity - originalSpeed;
-        Vector3 velocityChange = (targetVelocity - velocity);
-
-
-        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-        velocityChange.y = 0;
-
-        // Clamp to air control to not gain speed on each jump (due to original speed)
-        velocityChange = Vector3.ClampMagnitude(velocityChange, airControl);
-        rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-
-        if(gravityOn)
+        if(isGliding)
         {
-            if(isGliding)
-                rigidbody.AddForce(new Vector3 (0, -gravity * glideGravityMultiplier * rigidbody.mass, 0));
-            else
-                rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
+            GlideMethod2();            
         }
-
-        // Jump if not gliding
-        if (inputState.Jump.State == VButtonState.PRESS_START && isGliding == false) 
+        else
         {
-            // Start wall run if: Wall detected (in input direction), at correct angle and different than last wall normal.
-            // Also player needs to move forward to activate.
-            if(wallDetected 
-                && wallAngle > 90 && wallAngle < 140
-                && wallNormal != lastWallRunNormal 
-                && inputState.AxisInput.z > 0)
+            // Accelration Method
+            //rigidbody.AddForce(targetVelocity * airControl, ForceMode.VelocityChange);
+            
+            // Instant Method
+            targetVelocity *= airControl;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rigidbody.velocity - originalSpeed;
+            Vector3 velocityChange = (targetVelocity - velocity);
+
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            // Clamp to air control to not gain speed on each jump (due to original speed)
+            velocityChange = Vector3.ClampMagnitude(velocityChange, airControl);
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            if(gravityOn)
+                rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
+
+            // Jump if not gliding
+            if (inputState.Jump.State == VButtonState.PRESS_START && isGliding == false) 
             {
-                lastWallRunNormal = wallNormal;
-                // Reset jump button to avoid next state processing it
-                inputState.Jump.Set(VButtonState.UNPRESSED);
-                _stateMachine.SwitchState(6);
-                return;
-            }
-            // Wall jump
-            else if(wallDetected)
-            {
-                _stateMachine.SwitchState(2);
-            }
-            // Perform Air Jump if below maxAirJumps
-            else if(airJumps < maxAirJumps)
-            {
-                if(OnDoubleJumpStart != null) OnDoubleJumpStart();
-                airJumps++;
-                _stateMachine.SwitchState(2);
+                // Start wall run if: Wall detected (in input direction), at correct angle and different than last wall normal.
+                // Also player needs to move forward to activate.
+                if(wallDetected 
+                    && wallAngle > 90 && wallAngle < 140
+                    && wallNormal != lastWallRunNormal 
+                    && inputState.AxisInput.z > 0)
+                {
+                    lastWallRunNormal = wallNormal;
+                    // Reset jump button to avoid next state processing it
+                    inputState.Jump.Set(VButtonState.UNPRESSED);
+                    _stateMachine.SwitchState(6);
+                    return;
+                }
+                // Wall jump
+                else if(wallDetected)
+                {
+                    _stateMachine.SwitchState(2);
+                }
+                // Perform Air Jump if below maxAirJumps
+                else if(airJumps < maxAirJumps)
+                {
+                    if(OnDoubleJumpStart != null) OnDoubleJumpStart();
+                    airJumps++;
+                    _stateMachine.SwitchState(2);
+                }
             }
         }
 
@@ -211,7 +218,10 @@ public class AirState : PlayerState
                 return;
             }
 
-            DealFallDamageDistance();
+            // Deal fall damage if not gliding
+            if(isGliding == false)
+                DealFallDamageDistance();
+
             // GroundState
             _stateMachine.SwitchState(0);
         }
@@ -221,6 +231,7 @@ public class AirState : PlayerState
             _stateMachine.SwitchState(3);
     }
 
+    [System.Obsolete]
     private void DealFallDamageVelocity()
     {
         float velocity = transform.TransformVector(rigidbody.velocity).y;
@@ -240,8 +251,9 @@ public class AirState : PlayerState
     private void DealFallDamageDistance()
     {
         float delta = startY - transform.position.y;
-        if(delta < minimumFallDamageDistance) return;
-        int damage = Mathf.RoundToInt(FallDamage * (delta - minimumFallDamageDistance));
+        float fallHeight = fallDamageHeight.GetValue(characterStats);
+        if(delta < fallHeight) return;
+        int damage = Mathf.RoundToInt(FallDamage * (delta - fallHeight));
         characterStats.GetHit(null, damage * 2, damage, DamageType.Blunt, Direction9.CENTER);
     }
 
@@ -252,13 +264,17 @@ public class AirState : PlayerState
         targetDirection = transform.TransformDirection(inputVector);
         targetVelocity = targetDirection;
 
+        // Gliding
         if(inputState.Crouch.State == VButtonState.PRESSED && glideOn && isGliding == false)
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0f, rigidbody.velocity.z);
             isGliding = true;
         }
         else if(inputState.Crouch.State == VButtonState.UNPRESSED && isGliding)
+        {
+            startY = transform.position.y;
             isGliding = false;
+        }
     }
 
     #region Checks
@@ -269,4 +285,81 @@ public class AirState : PlayerState
     }
 
     #endregion
+
+    private void GlideMethod1()
+    {
+        // Gravity is always pulling down
+            Vector3 gravityVector = Vector3.down * gravity * rigidbody.mass;
+            
+            // Lift is up relative to glide transform and scales with down velocity
+            Vector3 liftVector = glideTransform.up * liftForce * -rigidbody.velocity.y;
+
+            // Get velocity on glideTransform.forward axis
+            Vector3 glideForwardVelocity = Vector3.Project(rigidbody.velocity, glideTransform.forward);
+            liftVector *= glideForwardVelocity.magnitude * .5f;
+
+            // 1 - facing up, 0.5 - facing forward, 0 - facing down
+            float dotProduct = Vector3.Dot(glideTransform.forward, Vector3.up) + 1; 
+            dotProduct *= .5f;
+
+            // Multiple lift by a curve (0 _/ 1 \_ 0) based on dot product
+            liftVector *= forwardCurve.Evaluate(dotProduct);
+            
+            // Clamp speed change on glideTransform forward
+            Vector3 finalVector = gravityVector + liftVector;
+            Vector3 targetForwardVelocity = Vector3.ProjectOnPlane(finalVector, glideTransform.forward);
+            finalVector -= targetForwardVelocity;
+
+            Vector3 forwardVelocityChange = targetForwardVelocity - glideForwardVelocity;
+            forwardVelocityChange.x = Mathf.Clamp(forwardVelocityChange.x, -maxVelocityChange, maxVelocityChange);
+            forwardVelocityChange.y = Mathf.Clamp(forwardVelocityChange.y, -maxVelocityChange, maxVelocityChange);
+            forwardVelocityChange.z = Mathf.Clamp(forwardVelocityChange.z, -maxVelocityChange, maxVelocityChange);
+
+            finalVector += forwardVelocityChange;
+
+            // Current velocity on xz axis
+            Vector3 xzVelocity = Vector3.ProjectOnPlane(rigidbody.velocity, Vector3.up);
+            
+            // Current glideTransform forward on xz axis
+            Vector3 xzGlideForward = Vector3.ProjectOnPlane(glideTransform.forward, Vector3.up);
+
+            // Clamp turn speed
+            Vector3 turnVector = xzGlideForward - xzVelocity;
+            turnVector.x = Mathf.Clamp(turnVector.x, -maxTurnSpeed, maxTurnSpeed);
+            turnVector.y = Mathf.Clamp(turnVector.y, -maxTurnSpeed, maxTurnSpeed);
+            turnVector.z = Mathf.Clamp(turnVector.z, -maxTurnSpeed, maxTurnSpeed);
+
+            rigidbody.AddForce(finalVector + turnVector);
+    }
+
+    private void GlideMethod2()
+    {
+        // Gravity is always pulling down
+        Vector3 gravityVector = Vector3.down * gravity * rigidbody.mass;
+        
+        // Lift is up relative to glide transform and scales with down velocity
+        Vector3 liftVector = glideTransform.up * liftForce * -rigidbody.velocity.y;
+
+        // 1 - facing up, 0 - facing forward, -1 - facing down
+        float dotProduct = Vector3.Dot(glideTransform.forward, Vector3.up); 
+
+        // More lift the more we face up
+        // 1 - Facing up, .5f Forward, 0 down
+        liftVector *= (dotProduct + 1) * .5f;
+
+        // Forward increases if we face forward
+        Vector3 forwardVector = glideTransform.forward * 10 * forwardCurve.Evaluate((dotProduct + 1) * .5f);
+
+        // Current velocity on xz axis
+        Vector3 xzVelocity = Vector3.ProjectOnPlane(rigidbody.velocity, Vector3.up);
+        
+        // Current glideTransform forward on xz axis
+        Vector3 xzGlideForward = Vector3.ProjectOnPlane(glideTransform.forward, Vector3.up);
+
+        // Clamp turn speed
+        Vector3 turnVector = xzGlideForward - xzVelocity;
+        turnVector = Vector3.ClampMagnitude(turnVector, maxTurnSpeed);
+
+        rigidbody.AddForce(gravityVector + liftVector + forwardVector + turnVector);
+    }
 }
